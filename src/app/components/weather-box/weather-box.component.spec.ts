90a973d78ab287883efe9c72cfd29ec28487b63e
Harrmalik <harrmalik@gmail.com>
2016-10-07 08:33:31 -0400

    new template with mock data

---
+++ src/app/components/weather-box/weather-box.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { WeatherBoxComponent } from './weather-box.component';
+
+describe('Component: WeatherBox', () => {
+  it('should create an instance', () => {
+    let component = new WeatherBoxComponent();
+    expect(component).toBeTruthy();
+  });
+});
